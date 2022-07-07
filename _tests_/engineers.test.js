45b43366a6026b22efe8e3355b019847a1f014af
Badara Dia <pushsoccer79@gmail.com>
2022-07-07 00:20:19 -0400

    Updated roles

--- _tests_/engineers.test.js
+++ _tests_/engineers.test.js
@@ -3,9 +3,10 @@ const Engineer = require('../lib/Engineer')
 test('test name, id,  email, github of Engineer', () => {
     
     //const github= 'Awa20'
-    const newEngineer = new Engineer('Awa', 20, 'Awa@email.com', 'Dia20')
+    const newEngineer = new Engineer('Awa', 20, 'Awa@email.com', 'Awa20')
     expect(newEngineer.name).toBe('Awa')
     expect(newEngineer.id).toBe(20)
     expect(newEngineer.email).toBe('Awa@email.com')
+    expect(newEngineer.github).toBe('Awa20')
     // expect(newEngineer.getGitHub).toBe(github)
 })
--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager')
 
-test('test name of manager', () => {
-    const name = 'Awa'
+test('test name, id, email, phone of manager', () => {
+    const newManger = new Manager('Awa'
     const newManager = new Manager(name)
     expect(newManager.name).toBe(name)
 })
