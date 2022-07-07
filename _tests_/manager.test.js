fa3426b6247392a3071ef987a7e5326defdef476
Badara Dia <pushsoccer79@gmail.com>
2022-07-07 00:37:22 -0400

    Library updates

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,7 +1,9 @@
 const Manager = require('../lib/Manager')
 
 test('test name, id, email, phone of manager', () => {
-    const newManger = new Manager('Awa'
-    const newManager = new Manager(name)
-    expect(newManager.name).toBe(name)
+    const newManger = new Manager('Moussa', 20, 'Moussa@email.com', '703-627')
+    expect(newManager.name).toBe('Moussa')
+    expect(newManager.id).toBe(20)   
+    expect(newManager.email).toBe('Moussa@email.com')
+    expect(newManger.phone).toBe('703-627')
 })
